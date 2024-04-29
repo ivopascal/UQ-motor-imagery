3f9cdbcfa317d6ae34d825aed300ba494508f79c
Joris Suurmeijer <jorissuurmeijer@gmail.com>
2024-04-29 20:23:43 +0200

    refactored DUQ and added Calibration plot

--- project/models/shallowConvNet/DUQ/SCN_train_DUQ.py
+++ project/models/shallowConvNet/DUQ/SCN_train_DUQ.py
@@ -1,7 +1,10 @@
 from keras.callbacks import EarlyStopping
+from keras_uncertainty.utils import classifier_calibration_error, classifier_calibration_curve, \
+    classifier_accuracy_confidence_curve
 from matplotlib import pyplot as plt
 from moabb.datasets import BNCI2014_001
 from moabb.paradigms import MotorImagery
+from sklearn.calibration import calibration_curve
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.utils import compute_sample_weight
@@ -14,6 +17,7 @@ from sklearn.preprocessing import LabelEncoder, normalize
 import numpy as np
 
 import seaborn as sns
+from tqdm import tqdm
 
 # from scipy.special import softmax
 from sklearn.utils.extmath import softmax
@@ -23,7 +27,31 @@ import warnings
 warnings.filterwarnings('ignore', category=FutureWarning)
 
 
-def evaluate_model(y_pred, y_true, subject_id):
+def evaluate_model(predictions, test_labels, subject_id):
+    predicted_classes = np.argmax(predictions, axis=1)
+    plot_and_evaluate(predicted_classes, test_labels, subject_id)
+
+    # Calculate probabilities to determine the confidence of the model
+    distances = normalize(predictions, axis=1, norm='l1')
+    temperature = 0.3  # This best followed the accuracy and F1 scores
+    prediction_proba = softmax(distances / temperature)
+
+    confidence = np.max(prediction_proba, axis=1)
+    overall_confidence = np.mean(confidence)
+    print("Overall Confidence: ", overall_confidence)
+
+    ece = classifier_calibration_error(predicted_classes, test_labels, confidence)
+    print("ECE: ", ece)
+
+    x, y = classifier_calibration_curve(predicted_classes, test_labels, confidence)
+    # classifier_accuracy_confidence_curve(predicted_classes, test_labels, confidence)
+
+    plt.plot(x, y, color='red')
+    plt.plot([0, 1], [0, 1], color='black')
+    plt.show()
+
+
+def plot_and_evaluate(y_pred, y_true, subject_id):
     subject_id = subject_id
     accuracy = accuracy_score(y_true, y_pred)
     print(f"Subject {subject_id} Validation accuracy: ", accuracy)
@@ -40,6 +68,7 @@ def evaluate_model(y_pred, y_true, subject_id):
     plt.show()
 
 
+
 def main():
     dataset = BNCI2014_001()        # load dataset
     paradigm = MotorImagery(        # make paradigm, filter between 7.5 and 30 Hz
@@ -54,7 +83,7 @@ def main():
     )
 
     num_subjects = 9
-    for subject_id in range(1, num_subjects + 1):       # loop to take data and make model per subject
+    for subject_id in tqdm(range(1, num_subjects + 1)):       # loop to take data and make model per subject
         subject = [subject_id]
 
         X, y, metadata = paradigm.get_data(dataset=dataset, subjects=subject)       # get the data for specific subject
@@ -76,7 +105,6 @@ def main():
         model = net.build()
 
         # weights = compute_sample_weight('balanced', y=y_train)
-
         model.fit(      # train the model
             X_train,
             y_categorical,
@@ -84,25 +112,14 @@ def main():
             epochs=200, batch_size=64, validation_split=0.1 #, sample_weight=weights
             ,verbose=0,
         )
-
         # model.save(f'../saved_trained_models/SCN/PerSubject/subject{subject_id}')
 
         label_encoder = LabelEncoder()
         test_labels = label_encoder.fit_transform(y_test)
 
+        # make predictions and test the model
         predictions = model.predict(X_test)
-
-        predicted_classes = np.argmax(predictions, axis=1)
-        evaluate_model(predicted_classes, test_labels, subject_id)
-
-        # Calculate probabilities to determine the confidence of the model
-        distances = normalize(predictions, axis=1, norm='l1')
-        temperature = 0.3       # This best followed the accuracy and F1 scores
-        prediction_proba = softmax(distances / temperature)
-
-        confidence = np.max(prediction_proba, axis=1)
-        overall_confidence = np.mean(confidence)
-        print("Overall Confidence: ", overall_confidence)
+        evaluate_model(predictions, test_labels, subject_id)
 
 
 
