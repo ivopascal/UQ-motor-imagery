eeb973607316c9b171f8e79b3dee6cef57cd8972
Joris Suurmeijer <jorissuurmeijer@gmail.com>
2025-04-27 16:58:36 +0200

    busy with making it better, temp commit

--- project/utils/rejection_coverage.py
+++ project/utils/rejection_coverage.py
@@ -32,25 +32,35 @@ def accuracy_coverage_curve(y_true, predicted, uncertainty, n_steps=50):
     # this way we reject atleast 1 sample and at most all samples, with n_steps sample in between
     # this does not guarantee that every step has a different number of datapoints that fall in the range of uncertainty
 
-    t_grid = np.linspace(uncertainty.min(), uncertainty.max(), n_steps, endpoint=True)
+    t_grid = np.linspace(uncertainty.min(), 1.0, n_steps, endpoint=True)
     coverages, accuracies = [], []
+
+    print("Uncertainty: ", uncertainty)
+    print("number of samples: ", len(uncertainty))
 
     for threshold in t_grid:
         # keep all samples that have less uncertainty (more confidence) than the threshold
         # so we reject samples with too high uncertainty
         keep = uncertainty <= threshold
+        print("threshold", threshold)
 
         n_kept = keep.sum()
+        print("n_kept", n_kept)
         min_points = 5
 
-        if n_kept < min_points:  # if too little datapoints, continue
-            continue
-
         cov  = keep.mean()
+        print("cov", cov)
 
-        if cov == 0.0:                 # if we reject all then skip
+        if cov == 0:                 # if we reject all then skip
+            # acc = 1.0
+            print("We rejected all, accuracy = 1")
             continue
-        acc  = accuracy_score(y_true[keep], predicted[keep].argmax(1))
+        else:
+            if n_kept < min_points:  # if too little datapoints, continue
+                print("Too little datapoints, threshold was: ", threshold)
+                continue
+            acc = accuracy_score(y_true[keep], predicted[keep].argmax(1))
+
         coverages.append(cov)
         accuracies.append(acc)   #1 - acc for risk
 
